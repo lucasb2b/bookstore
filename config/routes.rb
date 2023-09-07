134d48ba17286577fbb7ab1db6285bc1f51ca9d1
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-09-06 22:03:10 -0300

    Trying to make a login system into a system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "home#show"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/auth/login', to: 'sessions#new'
+  post '/auth/login', to: 'sessions#create'
+  delete 'auth/logout', to: 'sessions#destroy'
+
 end
