57f1c72df67fe32eacf5b7a73ea4e03b202a0eb0
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-09-06 23:41:25 -0300

    feat(bookstore): Login with email and password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      reset_session
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "olá " + user.email
+    else
+      redirect_to auth_login_path, alert: "Há algo de errado com suas credenciais, por favor verifique seus dados!"
+    end
   end
 
   def destroy
