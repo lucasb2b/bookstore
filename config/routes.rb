a3e6c2367ad74190afbc99eb810cca08b88b4f3c
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-09-15 21:22:06 -0300

    Reset application

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#show"
 
-  get '/auth/login', to: 'sessions#new'
-  post '/auth/login', to: 'sessions#create'
-  delete 'auth/logout', to: 'sessions#destroy'
+  #get '/auth/login', to: 'sessions#new'
+  #post '/auth/login', to: 'sessions#create'
+  #delete 'auth/logout', to: 'sessions#destroy'
 
 end
---
+++ test/factories/books.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :book do
+    author_id { 1 }
+    published_at { "2023-09-14 22:51:43" }
+  end
+end
